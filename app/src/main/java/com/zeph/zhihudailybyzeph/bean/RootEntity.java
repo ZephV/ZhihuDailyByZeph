a08ce10aff7a13034c3a1c16d8a48088a81aa240
ZephV <zephkwok@outlook.com>
2017-06-01 23:25:23 +0800

    Initial commit

---
+++ app/src/main/java/com/zeph/zhihudailybyzeph/bean/RootEntity.java
@@ -0,0 +1,16 @@
+package com.zeph.zhihudailybyzeph.bean;
+
+
+import java.util.ArrayList;
+
+public class RootEntity {
+  private ArrayList<StoriesEntity> stories;
+
+  public ArrayList<StoriesEntity> getStories() {
+    return stories;
+  }
+
+  public void setStories(ArrayList<StoriesEntity> stories) {
+    this.stories = stories;
+  }
+}
---
+++ app/src/main/java/com/zeph/zhihudailybyzeph/bean/StoryDetailsEntity.java
@@ -0,0 +1,42 @@
+package com.zeph.zhihudailybyzeph.bean;
+
+
+public class StoryDetailsEntity {
+
+  private String body;
+  private String image_source;
+  private String title;
+  private String image;
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
+  }
+
+  public String getImage_source() {
+    return image_source;
+  }
+
+  public void setImage_source(String image_source) {
+    this.image_source = image_source;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+}
